260757d091bbb6ec0d1d8b514aec307d42177fda
hinh27 <xuanhynk@gmail.com>
2021-01-26 15:07:54 +0700

    drawer menu

---
+++ lib/home/home_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      drawer: Drawer(),
+      body: Center(child: Text('Home view')),
+    );
+  }
+}
